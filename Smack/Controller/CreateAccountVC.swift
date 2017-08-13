add79fc8dff960539205cb71db3d7025c5e5b51b
Ryan Loera <rloera@csumb.edu>
2017-08-13 17:48:27 -0400

    Added login in function and used SwiftJSON to parse JSON data

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -29,7 +29,11 @@ class CreateAccountVC: UIViewController {
         guard let pass = passwordText.text, passwordText.text != "" else { return }
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
             if success {
-                print("Registered user!")
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
+                    if success {
+                        print("Logged in user!", AuthService.instance.authToken)
+                    }
+                })
             }
         }
     }
