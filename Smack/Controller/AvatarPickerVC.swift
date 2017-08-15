7ff346a99dc4f04fa20847bb4c72b1f32669e752
Ryan Loera <rloera@csumb.edu>
2017-08-14 20:54:22 -0400

    created AvatarCell class, AvatarPickerVC, added ability for user to create an account and stores in database

---
+++ Smack/Controller/AvatarPickerVC.swift
@@ -0,0 +1,43 @@
+//
+//  AvatarPickerVC.swift
+//  Smack
+//
+//  Created by RyLo on 8/14/17.
+//  Copyright © 2017 GBJpq. All rights reserved.
+//
+
+import UIKit
+
+class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var segemtnControll: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "avatarCell", for: indexPath) as? AvatarCell {
+            return cell
+        } else {
+            return AvatarCell()
+        }
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 28
+    }
+    
+    @IBAction func backPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func segmentControllChanged(_ sender: Any) {
+    }
+}
