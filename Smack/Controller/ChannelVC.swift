67c3ce36cabf3af64e3b42574ae9af4719912486
Ryan Loera <rloera@csumb.edu>
2017-08-22 20:47:44 -0400

    Added ProfileVC, created a small view for the user to see their information along with a logout button

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -11,6 +11,7 @@ import UIKit
 class ChannelVC: UIViewController {
     
     @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var userImage: CircleImage!
     
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {
         
@@ -20,10 +21,30 @@ class ChannelVC: UIViewController {
         super.viewDidLoad()
         //how far the revealViewController is from the right side when it slides over
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
     
     @IBAction func loginBtnPressed(_ sender: Any) {
-        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        if AuthService.instance.isLoggedIn {
+            //show Profile Page
+            let profile = ProfileVC()
+            profile.modalPresentationStyle = .custom
+            present(profile, animated: true, completion: nil)
+        } else {
+            performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        }
+    }
+    
+    @objc func userDataDidChange(_ notif: Notification) {
+        if AuthService.instance.isLoggedIn {
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImage.image = UIImage(named: UserDataService.instance.avatarName)
+            userImage.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        } else {
+            loginBtn.setTitle("Login", for: .normal)
+            userImage.image = UIImage(named: "menuProfileIcon")
+            userImage.backgroundColor = UIColor.clear
+        }
     }
     
 
