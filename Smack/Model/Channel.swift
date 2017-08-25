3e23d6c0876eae2b6d94f4cc702df5095425b915
Ryan Loera <rloera@csumb.edu>
2017-08-24 21:24:55 -0400

    Added MessageService and Channel files to parse channel data

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by RyLo on 8/24/17.
+//  Copyright © 2017 GBJpq. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
