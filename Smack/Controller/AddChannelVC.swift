3bd8757e43d212cf2dc4eb251b64e568c65f854c
Ryan Loera <rloera@csumb.edu>
2017-08-25 11:25:46 -0400

    created addChannelVC and xib to create channel name and description but no functionality to add them yet.

---
+++ Smack/Controller/AddChannelVC.swift
@@ -0,0 +1,40 @@
+//
+//  AddChannelVC.swift
+//  Smack
+//
+//  Created by RyLo on 8/25/17.
+//  Copyright © 2017 GBJpq. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+    
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var channelDescription: UITextField!
+    @IBOutlet weak var bgView: UIView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    @IBAction func closedModalPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func createChannelPressed(_ sender: Any) {
+    }
+    
+    func setupView() {
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(AddChannelVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+        
+        nameText.attributedPlaceholder = NSAttributedString(string: "name", attributes: [NSAttributedStringKey.foregroundColor : smackPurplePlaceholder])
+        channelDescription.attributedPlaceholder = NSAttributedString(string: "description", attributes: [NSAttributedStringKey.foregroundColor : smackPurplePlaceholder])
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+}
